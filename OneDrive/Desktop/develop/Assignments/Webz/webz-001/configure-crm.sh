4b75caeca4763e17e8ba0b31319a668c680af6ef
Eyal Meridan <eyal.meridan@scopiolabs.com>
2024-09-28 15:48:04 +0300

    add no-quorum-policy=ignore to crm

--- OneDrive/Desktop/develop/Assignments/Webz/webz-001/configure-crm.sh
+++ OneDrive/Desktop/develop/Assignments/Webz/webz-001/configure-crm.sh
@@ -8,6 +8,7 @@ crm configure primitive FloatingIP ocf:heartbeat:IPaddr2 \
 
 crm configure location FloatingIP-prefers-webz001 FloatingIP 100: webz-001 ### Set preference for webz-001
 crm configure location FloatingIP-prefers-webz002 FloatingIP 50: webz-002 ### Set failover option 1 to be webz-002
-crm configure location FloatingIP-prefers-webz003 FloatingIP 25: webz-003 ### Set failover option 2 to be webz-003
+crm configure location FloatingIP-prefers-webz003 FloatingIP 49: webz-003 ### Set failover option 2 to be webz-003
+crm configure property no-quorum-policy=ignore ### Make sure FloatingIP exists even if only one node is alive
 
 crm configure rsc_defaults resource-stickiness=100 ### This ensures that the floating IP will not move back to webz-001 immediately once it's back online unless forced.
